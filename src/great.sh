ad8cccde7d14b1a2761ec05df003e637f6e0c148
hxin <hxin87@gmail.com>
2020-01-21 15:23:43 +0000

    test

---
+++ src/great.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+https://meyerweb.com/eric/tools/dencoder/
+http://great.stanford.edu/help/display/GREAT/Programming+Interface#ProgrammingInterface-Examples
+http://bejerano.stanford.edu/great/public/cgi-bin/greatStart.php?requestURL=https%3A%2F%2Fraw.githubusercontent.com%2Fsidbdri%2FGREAT%2Fb1aff79b0264426f986bfaa6d583cdd734ddde4c%2Fdata%2Fmono_vs_co_20_mouse.bed&requestSpecies=mm10
+https://www.ensembl.org/Mus_musculus/Info/Index
+http://great.stanford.edu/public/html/index.php
